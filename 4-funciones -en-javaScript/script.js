3b09e64e46ed4627fce06f86bc922982cb4ac974
Emanuel Ledesma <emanuellescam@gmail.com>
2025-02-25 22:55:22 -0600

    Se agrega Proyecto de funciones, lección 4

--- 4-funciones -en-javaScript/script.js
+++ 4-funciones -en-javaScript/script.js
@@ -1,3 +1,4 @@
+/*
 function saludar1(nombre) {
     console.log("Hola, " + nombre + "!");
 }
@@ -56,4 +57,64 @@ saludar("María"); // Imprime: Hola, María!
 const multiplicar = (a, b) => a * b;
 
 console.log(multiplicar(4, 5)); // Imprime: 20
+*/
 
+//------------------------------------------------Ejercicios--------------------------------------
+/*
+//Ejercicio 1: Calculadora simple
+//Ejemplos de implementación
+
+function sumar (a, b) {
+return a + b;
+}
+let sumar1 = (a, b) => a + b;
+console.log(sumar1(13, 4));
+
+function restar(a, b) {
+    return a - b;
+}
+
+let restar1 = (a, b) => a - b;
+console.log(restar1(13,4));
+
+function multiplicar1(a, b) {
+    return a * b;
+}
+
+const multiplication = (a, b) => a *b;
+console.log(multiplication(14,4));
+
+function dividir(a, b) {
+    if (b === 0) {
+        return "Error: División por cero";
+    }
+    return a / b;
+}
+
+const dividir1 = (a,b) => b === 0 ? "Error: División por cero" : a/b;
+console.log(dividir1(14, 2)); // 7
+console.log(dividir1(10, 0)); // "Error: División por cero"
+
+// Llamadas a las funciones
+console.log("Suma: " + sumar(5, 3));
+
+console.log("Resta: " + restar(5, 3));
+
+console.log("Multiplicación: " + multiplicar1(5, 3));
+
+console.log("División: " + dividir(5, 0));
+*/
+
+//Ejercicio 2: Conversor de Temperaturas
+
+//Crea una función que convierta temperaturas entre grados Celsius y Fahrenheit.
+
+function celsiusAFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+}
+console.log("20°C en Fahrenheit es: " + celsiusAFahrenheit(20));
+
+function fahrenheitACelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5/9;
+}
+console.log("68°F en Celsius es: " + fahrenheitACelsius(68));
---
+++ 4-funciones -en-javaScript/seguimientos-libros.js
@@ -0,0 +1,19 @@
+// Crea un sistema muy sencillo para hacer seguimiento de los libros que has leído.
+let librosLeidos = [];
+
+function agregarLibro(titulo) {
+    librosLeidos.push(titulo);
+    return `"${titulo} se agrego a la lista"`
+}
+
+console.log(agregarLibro("El principito"));
+console.log(agregarLibro("Maze Runner"));
+
+
+function mostrarLibrosLeidos() {
+    return librosLeidos.length > 0
+    ? `Libros leídos: ${librosLeidos.join(", ")}`
+    : "Aun no has leido ningun libro.";
+}
+
+console.log(mostrarLibrosLeidos());
