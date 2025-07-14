d3d9f184c3fd6455b62a19da797a42ad0ded5fa1
Emanuel Ledesma <emanuellescam@gmail.com>
2025-07-14 11:55:37 -0600

    se realizaron cambios al proyecto manejo estado

--- 26-manejo-de-estado/manejo-estado/src/ListaCompras.jsx
+++ 26-manejo-de-estado/manejo-estado/src/ListaCompras.jsx
@@ -15,14 +15,14 @@ function ListaCompras() {
         if (nombre === "") return
 
         const existe = productos.some(
-            (prod) => prod.toLowerCase() === nombre);
+            (prod) => prod.nombre.toLowerCase() === nombre);
 
         if (existe) {
             alert("Este producto ya está en la lista.");
             return;
         }
 
-        setProductos([...productos, nuevoProducto]);
+        setProductos([...productos, { nombre: nuevoProducto, comprado: false }]);
         setNuevoProducto("");
 
     };
@@ -31,7 +31,7 @@ function ListaCompras() {
     const eliminarProducto = (index) => {
 
         const producto = productos[index];
-        const confirmar = window.confirm(`¿Seguro que quieres eliminar esto ${producto}?`)
+        const confirmar = window.confirm(`¿Seguro que quieres eliminar esto ${producto.nombre}?`);
 
         if (confirmar) {
             const productosRestantes = productos.filter((_, i) => i !== index);
@@ -39,16 +39,20 @@ function ListaCompras() {
         }
         // const productosRestantes = productos.filter((_, i) => i !== index);
         // setProductos(productosRestantes);
-
-
     };
 
     const guardarEdicion = (index) => {
         const nuevaLista = [...productos];
-        nuevaLista[index] = productoEditado;
+        nuevaLista[index].nombre = productoEditado;
         setProductos(nuevaLista);
         setEditandoIndex(null);
         setProductoEditado("");
+    };
+
+    const marcarComoComprado = (index) => {
+        const nuevaLista = [...productos]
+        nuevaLista[index].comprado = !nuevaLista[index].comprado;
+        setProductos(nuevaLista);
     };
 
     useEffect(() => {
@@ -79,21 +83,30 @@ function ListaCompras() {
             <button onClick={agregarProducto}>Agregar</button>
             <ul>
                 {productos.map((producto, index) => (
-                    <li key={index}>
+                    <li
+                        key={index}
+                        style={{ textDecoration: producto.comprado ? 'line-through' : 'none' }}
+                    >
+                        <input
+                            type="checkbox"
+                            checked={producto.comprado}
+                            onChange={() => marcarComoComprado(index)}
+                        />
                         {editandoIndex === index ? (
                             <>
                                 <input
-                                    value={productoEditado}
+                                    value={productoEditado.nombre}
                                     onChange={(e) => setProductoEditado(e.target.value)}
                                     onKeyDown={(e) => {
-                                        if (e.key === "Enter") guardarEdicion();
+                                        if (e.key === "Enter") guardarEdicion(index);
                                     }}
                                 />
+
                                 <button onClick={() => guardarEdicion(index)}> Guardar</button>
                             </>
                         ) : (
                             <>
-                                {producto}
+                                {producto.nombre}
                                 <button onClick={() => eliminarProducto(index)}> Eliminar</button>
                                 <button onClick={() => {
                                     setEditandoIndex(index);
