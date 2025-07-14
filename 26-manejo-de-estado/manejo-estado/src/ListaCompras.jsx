eb814be3ebabbfbd4b2c691f5e02162748bf5e48
Emanuel Ledesma <emanuellescam@gmail.com>
2025-07-14 11:03:32 -0600

    modificando lista de compras, entendiendo más useState

--- 26-manejo-de-estado/manejo-estado/src/ListaCompras.jsx
+++ 26-manejo-de-estado/manejo-estado/src/ListaCompras.jsx
@@ -1,9 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function ListaCompras() {
     // Definir el estado para la lista de compras
     const [productos, setProductos] = useState([]);
     const [nuevoProducto, setNuevoProducto] = useState("");
+
+    const [editandoIndex, setEditandoIndex] = useState(null);
+    const [productoEditado, setProductoEditado] = useState("");
 
     // Función para agregar un nuevo producto a la lista
     const agregarProducto = () => {
@@ -30,7 +33,7 @@ function ListaCompras() {
         const producto = productos[index];
         const confirmar = window.confirm(`¿Seguro que quieres eliminar esto ${producto}?`)
 
-        if(confirmar){
+        if (confirmar) {
             const productosRestantes = productos.filter((_, i) => i !== index);
             setProductos(productosRestantes);
         }
@@ -40,20 +43,64 @@ function ListaCompras() {
 
     };
 
+    const guardarEdicion = (index) => {
+        const nuevaLista = [...productos];
+        nuevaLista[index] = productoEditado;
+        setProductos(nuevaLista);
+        setEditandoIndex(null);
+        setProductoEditado("");
+    };
+
+    useEffect(() => {
+        const productosGuardados = localStorage.getItem("productos");
+        if (productosGuardados) {
+            setProductos(JSON.parse(productosGuardados));
+        }
+    }, [])
+
+
+    useEffect(() => {
+        localStorage.setItem("productos", JSON.stringify(productos));
+    }, [productos])
+
     return (
         <div>
             <h2>Lista de Compras</h2>
+            <p>Total de Productos: {productos.length}</p>
             <input
                 type="text"
                 value={nuevoProducto}
                 onChange={(e) => setNuevoProducto(e.target.value)}
+                onKeyDown={(e) => {
+                    if (e.key === "Enter") agregarProducto();
+                }}
+
             />
             <button onClick={agregarProducto}>Agregar</button>
             <ul>
                 {productos.map((producto, index) => (
                     <li key={index}>
-                        {producto}
-                        <button onClick={() => eliminarProducto(index)}>Eliminar</button>
+                        {editandoIndex === index ? (
+                            <>
+                                <input
+                                    value={productoEditado}
+                                    onChange={(e) => setProductoEditado(e.target.value)}
+                                    onKeyDown={(e) => {
+                                        if (e.key === "Enter") guardarEdicion();
+                                    }}
+                                />
+                                <button onClick={() => guardarEdicion(index)}> Guardar</button>
+                            </>
+                        ) : (
+                            <>
+                                {producto}
+                                <button onClick={() => eliminarProducto(index)}> Eliminar</button>
+                                <button onClick={() => {
+                                    setEditandoIndex(index);
+                                    setProductoEditado(producto);
+                                }}>Editar</button>
+                            </>
+                        )}
                     </li>
                 ))}
             </ul>
